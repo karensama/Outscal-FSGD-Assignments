c5b797894d0ae707b3bb20ec7daeae1f590e1407
Karen Samá <132864328+karensama@users.noreply.github.com>
2024-10-26 14:39:52 -0400

    Add files via upload

---
+++ Assignment 8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Assignment - Solving Fun Math Problem using Loops
+
+int main()
+{
+    int n, sum = 0;
+    cout<<"Type a natural number below:\n(Natural numbers are integers starting from 1 till infinity)"<<endl;
+    cin>>n;
+
+    for(int i = 1; i<=n; i++)
+    {
+        sum += i;
+    }
+    cout<<"The sum of the first "<<n<<" natural numbers is: "<<sum<<endl;
+
+}
+
+
